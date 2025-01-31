c84ab87ecd729853c084f3aabc75f5a769a289c9
aluleam <makingaberto69@gmail.com>
2025-01-31 09:18:00 -0600

    Fixed header

--- app.js
+++ app.js
@@ -171,3 +171,13 @@ window.addEventListener('scroll', () => {
       navbar.classList.remove('scrolled');
   }
 });
+
+const sidemenu = document.getElementById("sidemenu");
+
+function openmenu() {
+    sidemenu.classList.add("active");
+}
+
+function closemenu() {
+    sidemenu.classList.remove("active");
+}
